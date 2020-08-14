ada597bb6afab036ef79f298b932673f71105f26
mahadihasan1555 <mahadihasan1555@gmail.com>
2020-08-14 19:41:40 +0600

    Template String, multiple line string

---
+++ template.js
@@ -0,0 +1,9 @@
+//Template String, multiple line string
+
+const firstName = "Mahadi";
+const lastName = "Hasan";
+const fullName = firstName + " " + lastName + " "+ "is a good boy";
+
+//for easy way
+const fullName2 = `${firstName} ${lastName}`
+console.log(fullName2);
