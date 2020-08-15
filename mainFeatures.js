69670fa08adeb86ddd66759a0daf07a825799e55
mahadihasan1555 <mahadihasan1555@gmail.com>
2020-08-15 19:45:35 +0600

    ES6 Main Freatures

---
+++ mainFeatures.js
@@ -0,0 +1,14 @@
+// Let and Const
+// Template String
+// Arrow Functions 
+// Enhance Object
+// Destructuring 
+// Default Parameter
+// Map, Set
+// Spread Operator
+// Iterators 
+// Generators
+// Class 
+// Inheritance 
+// Modules 
+// Promises
